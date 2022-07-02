92f11f33924b61f98d201db20765057a44588e95
EunsilSon <eunsil1023@gmail.com>
2022-07-02 23:52:33 +0900

    Feat : 카드 작성, 카드 수정 기능 (DB 연동X) - 카드 작성 시 빈칸이면 경고문 띄움

--- Front-end/js/card.js
+++ Front-end/js/card.js
@@ -1,14 +1,27 @@
-const card_insert_btn = document.getElementById('card_insert_btn');
+const insert_btn = document.getElementById('insert_btn');
+const update_btn = document.getElementById('update_btn');
 
+
+if (document.getElementById("insert_btn")) {
+  insert_btn.addEventListener('click', () => insertCard());
+}
+
+if (document.getElementById("update_btn")) {
+  update_btn.addEventListener('click', () => updateCard());
+}
+
+if (document.getElementById("delete_btn")) {
+  update_btn.addEventListener('click', () => deleteCard());
+}
 
 // 카드 작성
-card_insert_btn.addEventListener('click', function(){
-  const card = document.getElementById('card');
+function insertCard() {
+  const cardContent = document.getElementById('card_content');
   
-  if (card.value == '') {
+  if (cardContent.value == '') {
     alert("카드를 작성하세요.");
   } else {
-    console.log("insert : " + card.value);
+    console.log("insert : " + cardContent.value);
 
   /*
   axios
@@ -18,4 +31,26 @@ card_insert_btn.addEventListener('click', function(){
   alert("카드가 작성되었습니다.");
   location.href="all_cards.html";
   }
-})
+}
+
+// 카드 수정
+function updateCard() {
+  const cardContent = document.getElementById('card_content');
+
+  console.log("update : " + cardContent.value);
+  /*
+  axios
+  카드 id, 내용
+  */
+
+  alert("카드가 수정되었습니다.");
+  location.href="card_detail.html"; // 수정한 카드로 이동
+}
+
+// 카드 삭제
+function deleteCard() {
+  // 카드의 id 를 가져온다
+  // 서버에 id를 보낸다
+  // 리턴 값을 받는다
+  alert("카드가 삭제되었습니다.");
+}
