18d431699f977d178c76786ffcd4cd1ff1a74287
EunsilSon <eunsil1023@gmail.com>
2022-07-19 02:11:31 +0900

    Feat : 로그인, 회원가입, id찾기, pw찾기 완성 (Axios 통신 완료)

--- Front-end/js/sign.js
+++ Front-end/js/sign.js
@@ -1,7 +1,6 @@
 const form = document.getElementById('form');
-const userId = document.getElementById('user_id');
-const userPw = document.getElementById('user_pw');
 const userName = document.getElementById('user_name');
+const userPw = document.getElementById('user_pw');
 const userTel = document.getElementById('user_tel');
 
 let isRight = true;
@@ -9,19 +8,14 @@ let isRight = true;
 // 입력 값 유효성 확인
 function checkInput(inputArr) {
 
-  if (inputArr.userId != "not existed") {
-    isRight = (inputArr.userId.length) >= 8 ? true : false;
-    // css : 8자 이상
-  }
-
-  if (inputArr.userPw != "not existed") {
-    isRight = (inputArr.userPw.length) >= 10 ? true : false;
-    // css : 10자 이상
-  }
-
   if (inputArr.userName != "not existed") {
     isRight = (inputArr.userName.length) >= 2 ? true : false;
     // css : 2자 이상
+  }
+
+  if (inputArr.userPw != "not existed") {
+    isRight = (inputArr.userPw.length) >= 8 ? true : false;
+    // css : 8자 이상
   }
 
   if (inputArr.userTel != "not existed") {
@@ -34,7 +28,6 @@ function checkInput(inputArr) {
 
 // 입력 값 지우기 
 function removeInput() {
-  userId.value = null;
   userPw.value = null;
   userName.value = null;
   userTel.value = null;
@@ -43,50 +36,33 @@ function removeInput() {
 // 로그인
 function signIn() {
 
-  axios({
-    method: 'get', //통신 방식
-    url: 'http://54.180.153.82:8000', //통신할 페이지
-  }, {withCredentials : true})
-    .then((Response)=>{
-    console.log(Response.data);
-}).catch((Error)=>{
-    console.log(Error);
-})
-
   let userInfoArr = {
-    userId : userId.value,
     userPw : userPw.value,
     userName : "not existed",
     userTel : "not existed",
   };
 
   if (checkInput(userInfoArr)) {
-  
-    /*
     axios({
-      method:'POST',
-      url:'',
+      method: 'post', //통신 방식
+      url: 'http://54.180.95.53:8000/sign_in',
       data: {
-        "user_id": id.value,
-        "user_pw": pw.value
+        "username": userName.value,
+        "password": userPw.value
       }
-    }).then((res) => {
-      console.log(res);
-
-      if (res) {
-      } else {
-        alert("아이디 및 비밀번호가 올바르지 않습니다.");
-      }
-
-    }).catch(error => {
-      console.log(error);
-    })
-    */
-   alert("로그인 되었습니다.");
-   form.submit();
-   removeInput();
-  } else {
-    alert("다시 입력하세요.");
+    }, { withCredentials : true })
+      .then((Response)=>{
+        if (Response.data == "fail") {
+          alert("아이디 및 비밀번호가 올바르지 않습니다.");  
+          removeInput();
+        } else {
+          alert("로그인 되었습니다.");
+          form.submit();
+          removeInput();
+        }
+  }).catch((Error)=>{
+      console.log(Error);
+  })
   }
 }
 
@@ -95,7 +71,6 @@ function signIn() {
 function signUp() {
 
   let userInfoArr = {
-    userId : userId.value,
     userPw : userPw.value,
     userName : userName.value,
     userTel : userTel.value,
@@ -103,18 +78,26 @@ function signUp() {
 
   if (checkInput(userInfoArr)) {
 
-    /*
-    axios ({
-
-    })
-    */
-
-    alert("회원가입 성공");
-
-    form.submit();
-    removeInput();
-  } else {
-    alert("다시 입력하세요.");
+    axios({
+      method: 'post', //통신 방식
+      url: 'http://54.180.95.53:8000/sign_up',
+      data: {
+        "username": userName.value,
+        "password": userPw.value,
+        "telephone": userTel.value
+      }
+    }, { withCredentials : true })
+      .then((Response)=>{
+        if (Response.data == "Existed") {
+          alert("존재하는 아이디입니다.");  
+        } else {
+          alert("회원가입에 성공했습니다.");
+          form.submit();
+          removeInput();
+        }
+  }).catch((Error)=>{
+      console.log(Error);
+  })
   }
 }
 
@@ -122,25 +105,30 @@ function signUp() {
 function findId() {
 
   let userInfoArr = {
-    userId : "not existed",
     userPw : "not existed",
-    userName : userName.value,
+    userName : "not existed",
     userTel : userTel.value,
   };
 
   if (checkInput(userInfoArr)) {
-
-    /*
-    axios ({
-
-    })
-    */
-    alert("회원님의 아이디는 _ 입니다.");
-
-    form.submit();
-    removeInput();
-  } else {
-    alert("다시 입력하세요.");
+    axios({
+      method: 'get', //통신 방식
+      url: 'http://54.180.95.53:8000/id',
+      params: {
+        "tel": userTel.value
+      }
+    }, { withCredentials : true })
+      .then((Response)=>{
+        if (Response.data == "fail") {
+          alert("일치하는 사용자가 없습니다.");  
+        } else {
+          alert("아이디는 " + Response.data + "입니다.");
+          form.submit();
+          removeInput();
+        }
+  }).catch((Error)=>{
+      console.log(Error);
+  })
   }
 }
 
@@ -148,7 +136,6 @@ function findId() {
 function findPw() {
 
   let userInfoArr = {
-    userId : userId.value,
     userPw : "not existed",
     userName : userName.value,
     userTel : userTel.value,
@@ -156,16 +143,24 @@ function findPw() {
 
   if (checkInput(userInfoArr)) {
 
-    /*
-    axios ({
-
-    })
-    */
-    alert("회원님의 비밀번호는 _ 입니다.");
-
-    form.submit();
-    removeInput();
-  } else {
-    alert("다시 입력하세요.");
+    axios({
+      method: 'get', //통신 방식
+      url: 'http://54.180.95.53:8000/pw',
+      params: {
+        "name": userName.value,
+        "tel" : userTel.value
+      }
+    }, { withCredentials : true })
+      .then((Response)=>{
+        if (Response.data == "fail") {
+          alert("일치하는 사용자가 없습니다.");  
+        } else {
+          alert("비밀번호는 " + Response.data + "입니다.");
+          form.submit();
+          removeInput();
+        }
+  }).catch((Error)=>{
+      console.log(Error);
+  })
   }
 }
